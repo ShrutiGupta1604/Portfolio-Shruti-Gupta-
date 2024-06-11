67747387142a7e5c93334438a796da14ad4e43a0
ShrutiGupta <ShrutiGupta1604@gmail.com>
2024-06-12 00:38:31 +0530

    portfolio

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
-import Footer from "./components/Footer"; 
+import Footer from "./components/Footer";
 import Experiences from "./components/Experience";
 
 function App() {
